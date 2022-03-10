fd085faef27b2e51df3c473e5d8c42cecf16f279
danjane <jdtjane@gmail.com>
2022-03-10 22:20:37 +0100

    Check files are roughly ok, and then.... what?

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+# Let's loop through the student files and check if they have correctly implemented factorial
+import os
+import importlib.util
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Where we find the student .py files
+student_folder = os.path.join(os.getcwd(), "StudentFiles")
+students = ["Einstein", "Leibniz", "Newton", "Gauss"]
+project = "Factorial"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def py_file_name(student):
+    return os.path.join(student_folder, project + "_" + student + ".py")
 
 
-# Press the green button in the gutter to run the script.
+def check_for_banned_word(py_file, bad_word):
+    with open(py_file, 'r') as searchfile:
+        for line in searchfile:
+            if bad_word in line.split():
+                return True
+
+
+def file_imports(py_file):
+    return check_for_banned_word(py_file, "import")
+
+
+def file_evals(py_file):
+    return check_for_banned_word(py_file, "eval")
+
+
+def check(file):
+    if not os.path.isfile(file):
+        print("File does not exist :(")
+        return False
+    if file_imports(file):
+        print("File contains the banned keyword 'import' :(")
+        return False
+    if file_evals(file):
+        print("File contains the banned keyword 'eval' :(")
+        return False
+    return True
+
+
+def test(py_file):
+    # Into the danger zone... we will load and run code from an unknown module.
+    # https://stackoverflow.com/a/67692
+    spec = importlib.util.spec_from_file_location("module.name", "/path/to/file.py")
+    foo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(foo)
+    foo.MyClass()
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    for student in students:
+        print(f"\nChecking student : {student}")
+        file = py_file_name(student)
+        if check(file):
+            test(file)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
